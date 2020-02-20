8e7459f6910b05b3f52dab99e4c5d6620eee97c6
eamonboyle <blaowskate@hotmail.com>
2020-02-20 16:37:31 +0000

    Add styling

--- src/containers/List.js
+++ src/containers/List.js
@@ -3,8 +3,42 @@ import React, { Component } from 'react';
 import Card from '../components/Card/Card';
 
 class List extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            loading: true,
+        };
+    }
+
+    async componentDidMount() {
+        const movies = await fetch('../../assets/data.json');
+        const moviesJson = await movies.json();
+
+        if (moviesJson) {
+            this.setState({
+                data: moviesJson,
+                loading: false
+            });
+        }
+    }
+
     render() {
-        return <Card />;
+        const { data, loading } = this.state;
+
+        if (loading) {
+            return <div>Loading...</div>
+        }
+
+        return (
+            <div className='row'>
+                {data.map(movie =>
+                    <div className="col-md-2">
+                        <Card key={ movie.id } movie={ movie } />
+                    </div>
+                )}
+            </div>
+        );
     }
 };
 
